2916478a128eea6bf984b15a9ddc12b25ba23c05
haihv96 <hai.hp.96@gmail.com>
2018-01-08 14:54:41 +0700

    brunch react with scss

--- brunch-config.js
+++ brunch-config.js
@@ -7,8 +7,28 @@ exports.files = {
   }
 };
 
+exports.modules = {
+  autoRequire: {
+    'app.js': ['initialize']
+  }
+};
+
 exports.plugins = {
   babel: {
     plugins: ['transform-class-properties']
   },
+  sass: {
+    mode: 'native',
+    sourceMapEmbed: true,
+    precision: 8,
+    modules: {
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+      ignore: ['app/styles/*.scss']
+    },
+    options: {
+      includePaths: [
+        'node_modules/normalize.css'
+      ]
+    }
+  }
 };
